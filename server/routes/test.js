4fbf51ae8ddd1189acc4fe38727363fe0319c78e
one <2093959501@qq.com>
2017-11-24 20:22:03 +0800

    注释遍历添加 添加了sql文件

--- server/routes/test.js
+++ server/routes/test.js
@@ -5,16 +5,16 @@ const router = require('koa-router')();
 
 router.get('/essay/id/:id', GetE.getEssayInfo);
 router.get('/essay/date/:date', GetE.getEssayByDate);
-router.get('/add/essay',GetE.addEssayInfo);
+// router.get('/add/essay',GetE.addEssayInfo);
 router.get('/new/essay',GetE.newEssayInfo);
 
 router.get('/one/vol/:vol', GetO.getOneIndexInfo);
-router.get('/add/one',GetO.addOneIndex);
+// router.get('/add/one',GetO.addOneIndex);
 router.get('/new/one',GetO.newOne);
 router.get('/one/date/:date',GetO.getOneByDate)
 
 router.get('/question/id/:id', GetQ.getQuestionById);
-router.get('/add/questions', GetQ.addQuestion);
+// router.get('/add/questions', GetQ.addQuestion);
 router.get('/question/date/:date', GetQ.getQuestionByDate);
 router.get('/new/question', GetQ.newQuestion);
 
